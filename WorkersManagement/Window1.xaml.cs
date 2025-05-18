4e5cad706cac297c9fac0bd7dd4a03e090158454
qrimosa <maratiskhakov9@email.com>
2025-05-18 15:47:45 +0200

    Window1 add icons/logic

--- WorkersManagement/Window1.xaml.cs
+++ WorkersManagement/Window1.xaml.cs
@@ -14,14 +14,40 @@ using System.Windows.Shapes;
 
 namespace WorkersManagement
 {
-    /// <summary>
-    /// Interaction logic for Window1.xaml
-    /// </summary>
     public partial class Window1 : Window
     {
         public Window1()
         {
             InitializeComponent();
+        }
+        private void LogoutButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Open login window (MainWindow), close current window
+            MainWindow loginWindow = new MainWindow();
+            loginWindow.Show();
+            this.Close();
+        }
+        private void CloseButton_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void EmployeeButton_Click(object sender, RoutedEventArgs e)
+        {
+            EmployeeWindow employeeWindow = new EmployeeWindow();
+            employeeWindow.Show();
+        }
+
+        private void ViewButton_Click(object sender, RoutedEventArgs e)
+        {
+            ViewWindow viewWindow = new ViewWindow();
+            viewWindow.Show();
+        }
+
+        private void SalaryButton_Click(object sender, RoutedEventArgs e)
+        {
+            SalaryWindow salaryWindow = new SalaryWindow();
+            salaryWindow.Show();
         }
     }
 }
