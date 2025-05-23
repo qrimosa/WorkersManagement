44d46b7326ac2b2e02a0451539a284f7bf652c9c
qrimosa <maratiskhakov9@email.com>
2025-05-23 23:51:29 +0200

    Implement View Window logic

--- WorkersManagement/ViewWindow.xaml.cs
+++ WorkersManagement/ViewWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -11,17 +12,64 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using WorkersManagement.Models;
 
 namespace WorkersManagement
 {
-    /// <summary>
-    /// Interaction logic for ViewWindow.xaml
-    /// </summary>
     public partial class ViewWindow : Window
     {
+        private readonly AppDbContext _context = new();
+
         public ViewWindow()
         {
             InitializeComponent();
+        }
+
+        private void HomeButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void SearchButton_Click(object sender, RoutedEventArgs e)
+        {
+            string empId = SearchIdTextBox.Text.Trim();
+
+            if (string.IsNullOrWhiteSpace(empId))
+            {
+                MessageBox.Show("Please enter a valid Employee ID.");
+                return;
+            }
+
+            var emp = _context.Employees.FirstOrDefault(x => x.EmployeeId == empId);
+
+            if (emp != null)
+            {
+                IdText.Text = emp.EmployeeId;
+                NameText.Text = emp.Name;
+                AddressText.Text = emp.Address;
+                GenderText.Text = emp.Gender;
+                PositionText.Text = emp.Position;
+                DOBText.Text = emp.DOB.ToString("yyyy-MM-dd");
+                PhoneText.Text = emp.Phone;
+                EducationText.Text = emp.Education;
+            }
+            else
+            {
+                MessageBox.Show("Employee not found.");
+                ClearDisplay();
+            }
+        }
+
+        private void ClearDisplay()
+        {
+            IdText.Text = "";
+            NameText.Text = "";
+            AddressText.Text = "";
+            GenderText.Text = "";
+            PositionText.Text = "";
+            DOBText.Text = "";
+            PhoneText.Text = "";
+            EducationText.Text = "";
         }
     }
 }
